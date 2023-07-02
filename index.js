b3925099552af9186791e0bbfc6f19eb20e88ec3
jerry44000 <shai.badarna@gmail.com>
2023-07-02 15:15:52 +0200

    devjson & testjson

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database.js');
 
-
 sequelize.sync();
 app.listen(3000, () => console.log('The app is running'));
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
   dialect: 'sqlite',
-  storage: './database.sqilite',
+  storage: './database.sqlite',
   logging: false,
 });
 
